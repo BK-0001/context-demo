01ecc315e5696dcd12c3e4f3d3e415998117596c
Bart Kang <bart.kang.0719@gmail.com>
2024-09-03 09:23:28 -0700

    add basic style and basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+
+const BASE_CLASS = "app";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={BASE_CLASS}>
+      {/* navbar */}
+      <div className={`${BASE_CLASS}__navbar`}>
+        <nav>
+          <ul>
+            <li>
+              <a href="">All Todos</a>
+            </li>
+            <li>
+              <a href="">Work</a>
+            </li>
+            <li>
+              <a href="">Study</a>
+            </li>
+          </ul>
+        </nav>
+        <button>New Category</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {/* todos */}
+      <div className={`${BASE_CLASS}__todos`}>Todos</div>
+      {/* title */}
+      {/* Todo Form*/}
+      {/* Todo List */}
+    </div>
+  );
 }
 
-export default App
+export default App;
